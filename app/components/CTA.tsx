50668868e41d49bdf713403486be7a424919bb82
Benjamin Charity <ben.charity@hey.com>
2024-03-01 09:37:25 -0500

    fix: overflow issue with logos on small screen

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -7,7 +7,7 @@ export function CTA() {
   return (
     <div className="mx-auto mt-20 flex max-w-5xl flex-col items-center rounded-lg border-[1px] border-black bg-black bg-gradient-to-r from-slate-800 via-purple-900 to-slate-800 p-8 text-center dark:bg-opacity-50 md:px-20 md:py-20">
       <h2 className="text-4xl tracking-tight text-white md:text-6xl">
-        Build better. Build faster.
+        Build better. Build <em>faster</em>.
       </h2>
 
       <p className="mt-4 text-lg text-white text-opacity-70 md:text-xl">
